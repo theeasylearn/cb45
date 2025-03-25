54a8b655a27839fc0994601f1da4b029e9886c83
theeasylearn <theeasylearn@gmail.com>
2025-03-25 18:38:36 +0530

    working with 2d array

--- 46_2darray_2.c
+++ 46_2darray_2.c
@@ -1,15 +1,45 @@
 // write a program to minimum sales and maximum value
 /*
+    1) create variable min to store minimum value & max to store maximum value
+    2) assume that 0th row and 0th column has minimum and maximum value
+    3) if value at 0th row and 1st column ls less then  minimum then store that value in minimum variable
+    4) if value at 0th row and 1st column ls greater then  maximum then store that value in maximum  variable
+    5) repeat step 3 and 4 for each and every row and column
+    6) display minimum and maximum value
 
+    findout & display name of item with minimum sales
+    findout & display name of item with maximum sales
 */
 #include <stdio.h>
 
 void main()
 {
     // create 2D ARRAY (matrix)
-    int sales[3][7] = {
-        {12, 25, 37, 44, 29, 18, 33},
-        {41, 23, 39, 50, 14, 27, 35},
-        {30, 47, 19, 45, 22, 38, 11}};
-    
+    int sales[3][7] =
+        {
+            /* pav bhaji */ {12, 25, 37, 44, 29, 18, 33},
+            /* pulav  */ {41, 23, 39, 50, 14, 27, 35},
+            /* dosa */ {30, 47, 19, 45, 22, 38, 11}};
+    int min, max, min_index, max_index;
+    min = sales[0][0];
+    max = sales[0][0];
+
+    for (int j = 0; j < 3; j++) // outer loop
+    {
+        for (int i = 1; i < 7; i++) // inner loop
+        {
+            if (sales[j][i] < min)
+            {
+                min = sales[j][i];
+                min_index = i;
+            }
+            if (sales[j][i] > max)
+            {
+                max = sales[j][i];
+                max_index = i;
+            }
+        }
+    }
+    printf("minimum sales = %d on the day %d maximum sales = %d on the day %d", min, (min_index + 1),max,(max_index + 1));
+
 }
---
+++ 46_2darray_3.c
@@ -0,0 +1,34 @@
+// write a program to findout  total sales of 3 branch item wise day wise (matrix addition) also findout maximum saleing item 
+/*
+    1) create array total sales to store 3 branch sales 
+    2) store addition of cell at 0row and 0 column of each branch in cell at 0th row 0th col in total sales 
+    3) repeat step 2 for each and every cell of 3 branches 
+    4) display sales 
+
+*/
+#include <stdio.h>
+
+void main()
+{
+    // create 2D ARRAY (matrix)
+    int branch_1_sales[3][7] =
+        {
+            /* pav bhaji */ {12, 25, 37, 44, 29, 18, 33},
+            /* pulav  */ {41, 23, 39, 50, 14, 27, 35},
+            /* dosa */ {30, 47, 19, 45, 22, 38, 11}};
+    int branch_2_sales[3][7] =
+        {
+            /* pav bhaji */ {12, 25, 37, 44, 29, 18, 33},
+            /* pulav  */ {41, 23, 39, 50, 14, 27, 35},
+            /* dosa */ {30, 47, 19, 45, 22, 38, 11}
+        };
+    int branch_3_sales[3][7] =
+        {
+            /* pav bhaji */ {12, 25, 37, 44, 29, 18, 33},
+            /* pulav  */ {41, 23, 39, 50, 14, 27, 35},
+            /* dosa */ {30, 47, 19, 45, 22, 38, 11}
+        };    
+    int total_
+    sales [3][7];
+
+}
