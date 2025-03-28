20fefea7deaa1afbab48ba3762894ef52a8156f4
theeasylearn <theeasylearn@gmail.com>
2025-03-28 18:30:03 +0530

    structure

--- 50_struct_2.c
+++ 50_struct_2.c
@@ -1,43 +1,61 @@
 // concept of array of structure
 /*
     write a program to accept 5 student name, age weight and gender
-
 */
 #include <stdio.h>
 #include <string.h>
 // definition of structure
 struct student
 {
-    char name[32];
-    int age;
-    float weight;
-    char gender;
-};
+    char name[33]; // array within structure 32
+    int age;       // 2
+    float weight;  // 4
+    char gender;   // 1
+    // 40
+}; // each structure type variable uses 40 byte in memory
+
 #define SIZE 5
 void main()
 {
     // create array of structure
     struct student list[SIZE];
     char msg[] = "Name = ";
-    int index=0;
-    while(index<SIZE)
+    int index = 0;
+    while (index < SIZE)
     {
         fflush(stdin);
-        printf("Enter %d detail ",index+1);
+        printf("Enter %d detail ", index + 1);
         printf("Enter name");
         gets(list[index].name);
         fflush(stdin);
 
         printf("Enter gender (m=male,f=female)");
         scanf("%c", &list[index].gender);
+        fflush(stdin);
 
         printf("Enter age");
         scanf("%d", &list[index].age);
+        fflush(stdin);
 
         printf("Enter weight");
         scanf("%f", &list[index].weight);
+        fflush(stdin);
+
         index++;
     }
 
-    //display array of structure
+    index = 0;
+    while (index < SIZE)
+    {
+        // display array of structure
+        printf("\n%d student detail\n",index+1);
+        printf("Name = %s",list[index].name);
+        printf("\nage = %d", list[index].age);
+        printf("\nweight = %.2f\n", list[index].weight);
+        if (list[index].gender == 'm')
+            printf("Male");
+        else
+            printf("Female");
+        index++;
+    }
 }
---
+++ 51_struct_3.c
@@ -0,0 +1,59 @@
+// write a program to create structure that has student name and 3 subject marks.
+// display result (name, marks, total, average, Pass or fail) findout who has secured 1st rank
+/*
+    write a program to accept 5 student name, age weight and gender
+*/
+#include <stdio.h>
+#include <string.h>
+// definition of structure
+struct resultsheet
+{
+    char name[33]; // array within structure 36
+    int marks[3];  // 9
+    int total;     // 2
+    float average; // 4
+    char PassFail; // 1
+}; // each structure type variable uses 50 byte in memory
+
+#define SIZE 3
+void main()
+{
+    // create array of structure
+    struct resultsheet list[SIZE];
+    int index = 0;
+    int row = 0;
+    while(row<SIZE)
+    {
+        printf("Enter %d student detail",row+1);
+        fflush(stdin);
+        printf("\nEnter Name");
+        gets(list[row].name);
+        printf("Enter marks in exam\n");
+        list[row].total = 0;
+        index=0;
+        while(index < 3)
+        {
+            printf("Enter %d subject marks", index + 1);
+            scanf("%d",&list[row].marks[index]);
+            list[row].total += list[row].marks[index];
+            list[row].average = list[row].total / 3;
+            if(list[row].marks[index] >= 35)
+            {
+                list[row].PassFail = 'P';
+            }
+            else
+            {
+                list[row].PassFail = 'F';
+            }
+            index++;
+        }
+        row++;
+    }
+    printf("\nName      Maths       Science     English     total       Percentage      Result \n");
+    row=0;
+    while(row<SIZE)
+    {
+        printf("\n%s            %d       %d     %d     %d       %.2f      %c \n", list[row].name, list[row].marks[0], list[row].marks[1], list[row].marks[2], list[row].total, list[row].average, list[row].PassFail);
+        row++;
+    }
+}
